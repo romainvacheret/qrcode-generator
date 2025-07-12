1393db006c8e0574295d0a043ae8d01d2beddbf9
Romain Vacheret <romain.vacheret@gmail.com>
2025-07-12 15:41:33 +0200

    chore: remove c project

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -xe
-
-clang -Wall -Wextra -lSDL2 -o main.out main.c \
-    src/qrcode.c src/pattern.c src/utils.c \
-    src/information.c src/encoding.c src/array.c \
-    src/log_antilog.c src/polynomial.c src/mask.c \
-    src/logger.c src/gui.c
--- src/gui.h
+++
@@ -1,8 +0,0 @@
-#ifndef GUI_H
-#define GUI_H
-
-#include "qrcode.h"
-
-void display_qrcode(QRCode* qrcode);
-
-#endif // GUI_H
